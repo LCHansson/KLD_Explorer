7d15fa6e38f88c9d67bad95776020d788d98490a
Love Hansson <love.hansson@gmail.com>
2012-11-22 23:26:45 +0100

    Stop reading data in load.project().

--- 1-init.R
+++ 1-init.R
@@ -2,7 +2,8 @@ library(ProjectTemplate)
 setwd("~/R/KLD_Explore")
 load.project()
 
-Metadata.Kolada <- read.csv("./data/Metadata_Kolada.csv")
+KLD_aldre1 <- read.csv("./data/kld_aldre1.csv", stringsAsFactors = FALSE)
+Metadata.Kolada <- read.csv("./data/Metadata_Kolada.csv", stringsAsFactors = FALSE)
 
 ##### LÄS IN KOLADA-DATA #####
 # (Ligger i ./data/, Görs automatiskt i project-filen)
@@ -13,16 +14,18 @@ Metadata.Kolada <- read.csv("./data/Metadata_Kolada.csv")
 Metadata.corr <- Metadata.Kolada[!duplicated(Metadata.Kolada$Kod),]
 
 # Merga på nyckeltal på äldredata
-aldre.long <- merge(kld.aldre1[c(2:5)], Metadata.corr[c(2,4)], by.x="Nyckeltal", by.y="Kortnamn", sort=TRUE, all.x=TRUE)
-aldre.long <- aldre.long[order(aldre.long$Kommun.Landsting),]
+aldre.long <- merge(KLD_aldre1[c(2:5)], Metadata.corr[c(2,4)], by.x="Nyckeltal", by.y="Kortnamn", sort=TRUE, all.x=TRUE)
 aldre.long[is.na(aldre.long)] <- 0
 aldre.long <- aldre.long[order(aldre.long$Kommun.Landsting),]
 
+## KLD_aldre1 innehåller nyckeltalsvärden som inte mappas upp från Metadata-setet.
+## Lös det tillfälligt genom att ta bort alla rader utan nyckeltal (==0).
+aldre.long <- aldre.long[aldre.long$Kod != 0,]
 
-aldre.wide <- dcast(data=aldre.long, formula=... ~ Kod , value.var="Visat.Värde")
+#aldre.wide <- dcast(data=aldre.long, formula=... ~ Kod , value.var="Visat.Värde")
 
 # Sortera och putsa aldre.nt
-colnames(aldre.long) <- c("nyckel", "NAME_2", "year", "value", "code")
+colnames(aldre.long) <- c("nyckel", "Kommun", "year", "value", "code")
 
 
 
--- 4-visualizations.R
+++ 4-visualizations.R
@@ -9,6 +9,6 @@ p <- ggplot(data=aldre.long[aldre.long$År==2011,], aes(x=Visat.Värde, y=Kommun
 
 p + geom_bar(aes(fill=Kod), position=position_stack())
 
-p <- ggplot(data=aldre.long, aes(x=Kommun.Landsting, y=Visat.Värde))
-p + geom_boxplot(group=aldre.long$År)
+p <- ggplot(data=aldre.long, aes(x=År, y=Visat.Värde))
+p + geom_boxplot(group=aldre.long$Kommun.Landsting, aes(fill=Kommun.Landsting))
 p + geom_boxplot()
