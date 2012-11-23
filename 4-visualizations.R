a5a1fbb71ed9a2209d1bff28e1b9fcb9f0b75082
Love Hansson <love.hansson@gmail.com>
2012-11-23 17:00:05 +0100

    Modified data, more graphs

--- 4-visualizations.R
+++ 4-visualizations.R
@@ -4,7 +4,9 @@
 #                                            #
 ##############################################
 
-
+##### EXPERIMENTELL KOD #####
+# Detta avsnitt är inte avsett för produktion utan för experiment.
+# Produktionskod återfinns nedan.
 p <- ggplot(data=aldre.long[aldre.long$year==2011,], aes(x=value, y=kommun))
 
 p + geom_bar(aes(fill=code), position=position_dodge())
@@ -16,6 +18,11 @@ p + geom_boxplot() + geom_point()
 p + geom_point()
 
 
-p <- ggplot(aldre.wide, aes(x=value.N21890, y=value.N23890))
+p <- ggplot(aldre.wide, aes(x=year, y=value.N21890))
+p + geom_bar(position=position_dodge(), stat="identity")
 
 p + geom_point(alpha=0.5) + geom_smooth(method="lm")
+
+
+p <- ggplot(bakgr.wide[bakgr.wide$År==c(2008,2009,2010),], aes(x=value.N00919, y=value.N00910))
+p + geom_point() + geom_smooth(method="lm")
