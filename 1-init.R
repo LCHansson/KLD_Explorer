4a15d9f0f658619b2ed35459c738892477e54085
Love Hansson <love.hansson@gmail.com>
2012-11-24 00:04:13 +0100

    Added helper function "loadcleandata" for import of  Kolada data (.csv)

--- 1-init.R
+++ 1-init.R
@@ -3,67 +3,19 @@ setwd("~/R/KLD_Explore")
 load.project()
 
 
-##### LÄS IN KOLADA-DATA #####
-# Äldredata
-KLD_aldre1 <- read.csv("./data/kld_aldre1.csv", stringsAsFactors = FALSE)
-
-# Misc data
-KLD_misc1 <- read.csv("./data/kld_randomdata.csv", stringsAsFactors = FALSE, encoding="ISO8859-1")
-
-# Enbart bakgrundsvariabler
-KLD_bakgr1 <- read.csv("./data/kld_bakgr.csv", stringsAsFactors = FALSE, sep=";", fileEncoding="ISO8859-1")
-
-## METADATA ##
+##### METADATA #####
 # Läs in nyckeltals-metadata
 Metadata.Kolada <- read.csv("./data/Metadata_Kolada.csv", stringsAsFactors = FALSE)
 
-
-##### DATATVÄTT #####
 # Ta bort dubletter från Metadata.Kolada
 # Av någon anledning är vissa nyckeltal duplicerade i filen, men det finns även
 # nyckeltal med olika kod men identiskt innehåll. Ta bort alla sådana dubletter.
 Metadata.corr <- Metadata.Kolada[!duplicated(Metadata.Kolada$Kod),]
 Metadata.corr <- Metadata.corr[!duplicated(Metadata.corr$Kortnamn),]
 
-# Ändra headers i inläst data
-colnames(KLD_bakgr1) <- c("Kommun", "Nyckeltal", "År", "value")
+##### LÄS IN KOLADA-DATA #####
 
+## NEDLADDAD DATA (format: long)
+bakgr <- loadcleandata("kld_bakgr")
+miscdata <- loadcleandata("kld_randomdata")
 
-# Merga på nyckeltals-metadata
-## TODO: Skriv om detta till en funktion?!?
-bakgr <- merge(KLD_bakgr1, Metadata.corr[c("Kortnamn", "Kod")], by.x="Nyckeltal", by.y="Kortnamn")
-bakgr[is.na(bakgr)] <- 0
-bakgr <- bakgr[order(bakgr$Kommun),]
-
-# Ta bort nyckeltalsnamn
-bakgr <- bakgr[c(2:5)]
-
-# Transformera från long till wide
-bakgr.wide <- reshape(bakgr, direction="wide", timevar="Kod", idvar=c("År", "Kommun"))
-
-
-
-
-
-
-
-
-aldre.long <- merge(KLD_aldre1[c(2:5)], Metadata.corr[c(2,4)], by.x="Nyckeltal", by.y="Kortnamn", sort=TRUE, all.x=TRUE)
-aldre.long[is.na(aldre.long)] <- 0
-aldre.long <- aldre.long[order(aldre.long$Kommun.Landsting),]
-
-## KLD_aldre1 innehåller nyckeltalsvärden som inte mappas upp från Metadata-setet.
-## Lös det tillfälligt genom att ta bort alla rader utan nyckeltal (==0).
-aldre.long <- aldre.long[aldre.long$Kod != 0,]
-
-
-# Sortera och putsa aldre.nt
-colnames(aldre.long) <- c("nyckel", "kommun", "year", "value", "code")
-
-# Transponera till wide
-aldre.wide <- reshape(aldre.long, direction="wide", timevar="code", idvar=c("year", "kommun"))
-
-
-# Hämta ut data för ett visst år och en viss variabel
-aldre.2010 <- aldre.nt[aldre.nt$year == '2010',]
-aldre.2010.N1 <- aldre.2010[aldre.2010$code == 'N23890',]
--- lib/helpers.R
+++ lib/helpers.R
@@ -2,3 +2,28 @@ helper.function <- function()
 {
   return(1)
 }
+
+loadcleandata <- function(title)
+{
+  typeof(title)
+  path <- paste("./data/", title, ".csv", sep="")
+  sprintf(path)
+  # Loads and cleans data from the ./data folder.
+  # Data must be in csv (sep=";") format and encoded in ISO8859-1.
+  x <- read.csv(path, sep=";", stringsAsFactors = FALSE, fileEncoding="ISO8859-1")
+  
+  # Add metadata
+  x <- merge(x, Metadata.corr[c("Kortnamn", "Kod")], by.x="Nyckeltal", by.y="Kortnamn")
+
+  # Drop "Nyckeltal" column
+  x <- x[,!colnames(x) %in% "Nyckeltal"]
+  
+  # Clean NAs and order data for browsability
+  x[is.na(x)] <- 0
+  x <- x[order(x$Kommun),]
+  
+  # Rename columns
+  colnames(x) <- c("Kommun", "År", "value", "Kod")
+  
+  return(x)
+}
