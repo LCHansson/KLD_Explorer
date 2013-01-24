919ccb67707d22f5f8c2566d3e9d5e1b1f75e3eb
Love Hansson <love.hansson@gmail.com>
2013-01-25 00:18:29 +0100

    Implementerade Coldbir för alla databasanrop. Laddar nu mer data i INIT-fasen, vilket snabbar upp kartfunktionen ordentligt.

--- server.R
+++ server.R
@@ -9,7 +9,21 @@ kommunNamn <- sort(kommunNamn)
 # Create dimensional data frame
 commune <- wide_data$get_v("Kommun")
 year <- wide_data$get_v("År")
-dim_df <- data.table(Kommun=commune, År=year)
+dim_DT <- data.table(Kommun=commune, År=year, key="Kommun")
+
+kom_key <- get_lookup("Kommun", "./db/wide")
+
+# Load Sweden Shapefile
+sverige = readOGR(dsn="./mapdata/Kommuner_SCB/", layer="Kommungranser_SCB_07", input_field_name_encoding="ISO8859-1")
+sverige@data$id = rownames(sverige@data)
+sverige.points = fortify(sverige, region="id")
+sverige.df = join(sverige.points[,c("long", "lat", "group", "id")], sverige@data[,c("KNNAMN", "id")], by="id")
+
+# Set gpclibPermitStatus() to TRUE
+# (Whatever that means...)
+
+gpclibPermit()
+
 
 shinyServer(function(input, output) {
   
@@ -17,19 +31,20 @@ shinyServer(function(input, output) {
   ##          Internal functions                                     ##
   #####################################################################
   getOutputData <- reactive(function() {
-    gg_df <- dim_df
+    DT <- copy(dim_DT)
     
     xvar <- wide_data$get_v(input$category)
-    gg_df$var1 <- xvar
     
     yvar <- wide_data$get_v(input$categ2)
-    gg_df$var2 <- yvar
     
     xname <- input$category
     yname <- input$categ2
-    setnames(gg_df, 1:4, c("Kommun", "År", xname, yname))
     
-    gg_df
+    DT[,c("xvar", "yvar") := list(xvar, yvar)]
+    
+    setnames(DT, names(DT), c("Kommun", "År", xname, yname))
+    
+    return(DT)
   })
   
   
@@ -49,54 +64,50 @@ shinyServer(function(input, output) {
   ##          TIME SERIES PLOT                                       ##
   #####################################################################
   output$timeseries_plot <- reactivePlot(function() {
-    #     browser()
     
-    gg_df <- getOutputData()
-
-    kom_key <- get_lookup("Kommun", "./db/wide")
+    gg_DT <- getOutputData()[subset(kom_key, value == c(input$kommun))["value"]]
     
-    setnames(gg_df, c(input$category, input$categ2), c("primary", "secondary"))
-    
-    # TODO: Change from KLData to the Coldbir wide db
-    # (This might require implementing Coldbir Dimensions to data)
-    p <- ggplot(data=gg_df, 
-                aes(x=År,
-                    y=primary,
-                    xmin=min(allyears), 
-                    xmax=max(allyears)
-                ),
-                subset=.(Kommun == 5),
+    p <- ggplot(data=gg_DT,
                 group=1
-    )
-    
-#     browser()
+    ) + 
+      aes_string(x="År",
+                 y=input$category,
+                 xmin=min(allyears), 
+                 xmax=max(allyears)
+      )
     
     p <- p + layer(
       geom=input$graftyp
     )
     
-    
     # Kod för utvärdering av två variabler med samma y-limits
     if(input$tvavar) {
       p <- p + layer(
-        geom=input$graftyp, 
-        subset=.(Variabelkod == input$categ2 & Kommun == input$kommun), 
-        aes(color="red", fill="red")
+        geom=input$graftyp,
+        aes(color="red", fill="red"),
+        mapping=aes_string(x="År",
+                           y=input$categ2)
       )
       
       if (input$smooth) {
-        p <- p + geom_smooth(method=ifelse(input$loess == TRUE, "loess", "lm"), subset = .(Variabelkod == input$categ2 & Kommun == input$kommun), aes(group=1,color="red"))
+        p <- p + geom_smooth(
+          method=ifelse(input$loess == TRUE, "loess", "lm"), 
+          mapping=aes_string(x="År", y=input$categ2),
+          color="red"
+        )
       }
     }
     
     if (input$smooth) {
-      p <- p + geom_smooth(method=ifelse(input$loess == TRUE, "loess", "lm"), subset = .(Variabelkod == input$category & Kommun == input$kommun), aes(group=1))
+      p <- p + geom_smooth(
+        method=ifelse(input$loess == TRUE, "loess", "lm"), 
+        mapping=aes_string(x="År", y=input$category)
+      )   
     }
     
     p <- p + ggtitle(input$kommun)
     
     print(p)
-    print(paste("Kommun:", input$kommun))
   })
   
   
@@ -106,13 +117,13 @@ shinyServer(function(input, output) {
   #####################################################################
   output$twoway_plot <- reactivePlot(function() {  
     
-    gg_df <- getOutputData()
+    gg_DT <- getOutputData()
     
-    q <- ggplot(data=gg_df, aes_string(x=input$category, y=input$categ2, color="Kommun"))
+    q <- ggplot(data=gg_DT, aes_string(x=input$category, y=input$categ2, color="Kommun"))
     q <- q + layer(geom="point", subset=.(År == input$year))
     
     
-    # TODO: Feetch this data from the Coldbir Metadata db
+    # TODO: Fetch this data from the Coldbir Metadata db
     q <- q + labs(
       x=Metadata[Metadata$Kod == input$category, "Kortnamn"],
       y=Metadata[Metadata$Kod == input$categ2, "Kortnamn"]
@@ -142,22 +153,13 @@ shinyServer(function(input, output) {
   
   output$map_plot <- reactivePlot(function() {
     
-    # Set gpclibPermitStatus() to TRUE
-    # (Whatever that means...)
-    gpclibPermit()
+    gg_DT <- subset(getOutputData(), År == input$year)[,c("Kommun", input$category), with=F][kom_key]
+        
+    # Merge shapefile with KLD data
+    setnames(gg_DT, names(gg_DT), c("Kommun", "Värde", "KNNAMN"))    
+    sverige.df.KL <- join(sverige.df, gg_DT, by="KNNAMN", type="left")
     
-    # Load Sweden Shapefile
-    sverige = readOGR(dsn="./mapdata/Kommuner_SCB/", layer="Kommungranser_SCB_07", input_field_name_encoding="ISO8859-1")
-    sverige@data$id = rownames(sverige@data)
-    sverige.points = fortify(sverige, region="id")
-    sverige.df = join(sverige.points, sverige@data, by="id")
-    
-    # TODO: Change from KLData to the Coldbir db.
-    # Merge with KLD data
-    KL_mergeframe <- KLData[KLData$Variabelkod == input$category & KLData$År == input$year ,c("Kommun", "Värde")]
-    setnames(KL_mergeframe, 1:2, c("KNNAMN", "Värde"))
-    sverige.df.KL <- join(sverige.df, KL_mergeframe, by="KNNAMN", type="left")
-    
+#     browser()
     
     # Plot Sweden map
     p <- ggplot(sverige.df.KL, na.rm=TRUE) + 
@@ -169,7 +171,8 @@ shinyServer(function(input, output) {
       theme_bw() +
       theme(axis.title = element_blank(),
             axis.text = element_blank(),
-            axis.ticks = element_blank())
+            axis.ticks = element_blank()
+      )
     
     print(p)
     
