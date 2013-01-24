c5abb1fc4771af1fcc710ee323eb5dffc6486a88
Love Hansson <love.hansson@gmail.com>
2013-01-25 00:37:35 +0100

    Fixar bugg med gpclibPermit()

--- server.R
+++ server.R
@@ -13,17 +13,16 @@ dim_DT <- data.table(Kommun=commune, År=year, key="Kommun")
 
 kom_key <- get_lookup("Kommun", "./db/wide")
 
-# Load Sweden Shapefile
+## Create Sweden map data
+# Set gpclibPermitStatus() to TRUE
+# (Whatever that means...)
+gpclibPermit()
+
+# Load Shapefile
 sverige = readOGR(dsn="./mapdata/Kommuner_SCB/", layer="Kommungranser_SCB_07", input_field_name_encoding="ISO8859-1")
 sverige@data$id = rownames(sverige@data)
 sverige.points = fortify(sverige, region="id")
 sverige.df = join(sverige.points[,c("long", "lat", "group", "id")], sverige@data[,c("KNNAMN", "id")], by="id")
-
-# Set gpclibPermitStatus() to TRUE
-# (Whatever that means...)
-
-gpclibPermit()
-
 
 shinyServer(function(input, output) {
   
